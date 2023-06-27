88bc77aadfef85669fcada44b75fd5d66b0bb927
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-27 11:44:57 -0600

    updates

--- app/posts/new/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>New Posts</div>;
-};
-
-export default page;
--- app/posts/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Posts</div>;
-};
-
-export default page;
