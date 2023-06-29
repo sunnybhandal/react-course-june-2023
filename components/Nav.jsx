c2d4105198247a6321e73d89f671dabe45e47d62
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-29 15:52:03 -0600

    basic provider setup

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,18 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
   const isUserLoggedIn = true;
 
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+
+    setProviders();
+  }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -18,33 +30,48 @@ const Nav = () => {
           className="object-contain"
         />
         <p className="logo_text">Promptopia</p>
-
-        {/* Mobile Navigation */}
-        <div className="sm:flex hidden">
-          {isUserLoggedIn ? (
-            <div className="flex gap-3 md:gap-5">
-              <Link href="/create-prompt" className="black_btn">
-                Create Post
-              </Link>
-              <button type="button" onClick={signOut} className="outline_btn">
-                {" "}
-                Sign Out
-              </button>
-              <Link href="/profile">
-                <Image
-                  src="/assets/images/profile.svg"
-                  width={37}
-                  height={37}
-                  className="rounded-full"
-                  alt="profile"
-                />
-              </Link>
-            </div>
-          ) : (
-            <></>
-          )}
-        </div>
       </Link>
+
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              {" "}
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div></div>
     </nav>
   );
 };
