e5f4c9e1a92c1468a3009429e08c8d0545e6fde4
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-30 10:52:33 -0600

    create prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
@@ -12,6 +14,26 @@ const CreatePrompt = () => {
   });
   const createPrompt = async (e) => {
     e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        Router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
