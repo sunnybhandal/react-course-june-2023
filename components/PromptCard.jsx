d6ac0f6b802a36e6eef1e3b79144163f698811ef
sunnybhandal <sunnybhandal123@gmail.com>
2023-07-12 17:20:38 -0600

    prompt card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const router = useRouter();
+
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-enter">
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div>
+            <h3>{post.creator.username}</h3>
+            <p>{post.creator.email}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PromptCard;
