ff30127393238811eea040e9aa14f2e86d8ea403
sunnybhandal <sunnybhandal123@gmail.com>
2023-07-13 14:25:03 -0600

    s

---
+++ components/Profile.jsx
@@ -0,0 +1,11 @@
+import PromptCard from "./PromptCard";
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section>
+      <h1>{name}</h1>
+    </section>
+  );
+};
+
+export default Profile;
